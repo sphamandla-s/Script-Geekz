884d14263e883ab9900803487f149cdf20cb030d
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2022-11-17 10:17:55 +0200

    done with the nav-bar

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('#header .toggle-button');
+const collapse = document.querySelectorAll('#header .collapse');
+
+toggle.addEventListener('click', ()=>{
+    collapse.forEach(e => {
+        e.classList.toggle('collapse-toggle')
+    });
+});
